6772f429311ebb7546b40970ffd76adbbe02bfd5
paypaypay <turanbiubiubiu@gmail.com>
2021-03-22 16:05:12 +0800

    rename index

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@ export default {
       let that = this;
       // this.qrcode()
       let params = "?env=production";
-      params +=  getQueryString("order_id") ? "&order_id=" + getQueryString("index") : "";
+      params +=  getQueryString("order_id") ? "&order_id=" + getQueryString("order_id") : "";
       params +=  getQueryString("amount") ? "&amount=" + getQueryString("amount") : "";
       axios.get("/address" + params).then(function (response) {
         console.log(response.data);
